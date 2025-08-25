33ff2397642b472da0d603ccf8bf24d6b32475dc
solehudin11001 <solehudin11001@gmail.com>
2025-08-25 14:26:34 +0700

    Fix: Fix style error

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,12 @@ module.exports = {
     'browser': true,
     'es6': true,
     'jest/globals': true,
-    'node': true
+    'node': true,
   },
   'extends': [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:cypress/recommended'
   ],
   'parserOptions': {
     'ecmaFeatures': {
@@ -17,7 +18,7 @@ module.exports = {
     'sourceType': 'module'
   },
   'plugins': [
-    'react', 'jest'
+    'react', 'jest', 'cypress'
   ],
   'rules': {
     'indent': [
